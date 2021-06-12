344b302c6cc4b7cbe7bed311ca1df898825da040
Briankaweesi3 <burianwork@gmail.com>
2021-06-12 11:46:41 +0300

    main

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -53,11 +53,6 @@ $(function () {
                             "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
                         )
                         .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(
-                            "Sorry " +
-                            firstName +
-                            ", it seems that Our mail server is not responding. Please try again later!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
