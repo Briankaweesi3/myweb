acc1cea35741a7442ddd10e565558844a4cdc6b2
Briankaweesi3 <burianwork@gmail.com>
2021-06-16 11:13:14 +0300

    Update contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,11 +1,12 @@
 <?php
-
-Email sending method: SMTP;
-SMTP authentication: enabled;
-SMTP username: craftedug@gmail.com;
-SMTP password: 0787499613;
-SMTP hostname: smtp.gmail.com;
-SMTP port: 465 with SSL on.
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'craftedug@gmail.com';                 // SMTP username
+$mail->Password = '0787499613';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
 
 // Check for empty fields
 if(empty($_POST['name'])      ||
